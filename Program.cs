0b421c18ee0a459f6530cd91f865cfbaef64ab97
Regis <regis.lopes.eso@gmail.com>
2024-12-13 16:38:53 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using tabuleiro;
+
+
+
+namespace xadrez_console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine(P);
+
+
+        }
+    }
+}
